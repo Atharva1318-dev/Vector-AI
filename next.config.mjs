442d095e3a2a045fa197814a6f1f9ac3dbf6c227
Atharva <atharvajofficial@email.com>
2025-12-29 18:41:36 +0530

    fix prisma error occured during deployement

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/*': ['./node_modules/.prisma/client/*.wasm', './node_modules/.prisma/client/*.so.node'],
+    },
+  },
 };
 
 export default nextConfig;
